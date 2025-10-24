e684a1156decf5d9e98b74f2f95021b70b3fc886
CodeGab <139085745+gabriellefarahpy@users.noreply.github.com>
2025-10-23 21:11:54 -0400

    Update

--- Card.java
+++ Card.java
@@ -1,4 +1,3 @@
-
 public class Card{
     private Color color;
     private Type type;
@@ -11,18 +10,29 @@ public class Card{
         this.number = number;
 
         public void setOtherSide(Card other){
-
+            this.otherSide = other;
         }
 
         public Card getOtherSide(){
+            return otherSide;
 
         }
         public boolean matches(Card other){
-
+            if (color ==other.color || this.type == Type.NUMBER && other.type == Type.NUMBER && this.number == other.number){
+                return true;
+            }
+            if (this.type==other.type || this.type == Type.WILD || this.type == Type.WILD_DRAW_FOUR){
+                return true;
+            }
+            return false;
         }
-
+        @Override
         public String toString(){
-            
+        if (type == Type.NUMBER) {
+            return color + " " + number;
+        } else {
+            return color + " " + type;
+        }
         }
     }
 }
